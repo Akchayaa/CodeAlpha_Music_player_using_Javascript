11d9ca177c9df83424f4700092aa8e9cecb2b2d4
Akchayaa <143854669+Akchayaa@users.noreply.github.com>
2024-07-27 21:17:43 +0530

    Add files via upload

---
+++ task4/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Kadhal Endral",
+        artist: "Yuvan shankar raja",
+        img: "kadhal endral",
+        src: "kadhal endral"
+    },
+    {
+        name: "Mazhai Vara Poguthe",
+        artist: "Harris jayaraj",
+        img: "mazhai vara poguthe",
+        src: "mazhai vara poguthe"
+    },
+    {
+        name: "Munbe Vaa",
+        artist: "Shreya goshal",
+        img: "munbe vaa",
+        src: "munbe vaa"
+    },
+    {
+        name: "Neeye Neeye",
+        artist: "Srikanth deva",
+        img: "neeye neeye",
+        src: "neeye neeye"
+    },
+    {
+        name: "Ranjithame",
+        artist: "Manasi",
+        img: "ranjithame",
+        src: "ranjithame"
+    },
+    {
+        name: "Rowdy Baby",
+        artist: "Dhanush",
+        img: "rowdy baby",
+        src: "rowdy baby"
+    }
+];
